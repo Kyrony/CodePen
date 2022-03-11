9d3e4c291196d477b7e91b3b60622aa9dea974fc
Kyle Bruns <Kylecbruns@live.com>
2022-03-10 19:24:49 -0800

    only commit

---
+++ FooterCodePen/main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            foot: ['About','Products','Contact'],
+            options: ['place','thing','style','more','stuff'],
+        }
+    }
+})
